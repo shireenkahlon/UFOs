dcfd77df3c6b32e80ec7398ba3b3be0d3d6cb6e4
shireenkahlon <75495212+shireenkahlon@users.noreply.github.com>
2021-03-18 13:16:32 -0500

    Added Bootstrap styling to HTML file. Created and added sCSS style sheet

--- static/js/app_1.js
+++ static/js/app_1.js
@@ -7,11 +7,11 @@ function buildTable(data) {
     tbody.html("");
 // Next, loop through each object in the data
 // and append a row and cells for each value in the rowdata.forEach((dataRow) => {
-    data.forEach((dataRow) {
+    data.forEach(function(dataRow) {
         let row=tbody.append('tr')
         // Loop through each field in the dataRow and add
         // each value as a table cell (td)
-        Object.values(dataRow).forEach((val) {
+        Object.values(dataRow).forEach(function(val) {
             let cell = row.append('td')
             cell.text(val)
         }
